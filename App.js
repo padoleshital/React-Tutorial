3458550ae44901d19a5c6f21eaf8b7cd191766d3
padoleshital <shitalpadole7@gmail.com>
2023-11-08 18:45:17 +0530

    episode-2 Undestanding packages
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", 
-{
-    id:"heading",xyz:"abc"
-} ,
-'This is First React Application!');
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am at h1 tag"),
+    React.createElement("h2", {}, "I am at h1 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am at h1 tag"),
+    React.createElement("h2", {}, "I am at h1 tag"),
+  ]),
 
-console.log("Heading",heading)  //return object 
+]);
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
