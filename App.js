18844dfd7e51e58d0af2cd1ca8687516749346ca
padoleshital <shitalpadole7@gmail.com>
2023-11-08 19:55:01 +0530

    Episode-3 /part-3
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am at h1 tag"),
-    React.createElement("h2", {}, "I am at h1 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am at h1 tag"),
-    React.createElement("h2", {}, "I am at h1 tag"),
-  ]),
-
-]);
-console.log(parent);
+const heading = React.createElement("h1", {id:"heading"},"Hello shital");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading)
+
+
