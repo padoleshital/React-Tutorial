f90e6ce4a46cb689bbc2b251d13e3876a2832d28
padoleshital <shitalpadole7@gmail.com>
2023-11-09 20:16:33 +0530

    episode-3 ReactElements
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id:"heading"},"Hello shital");
+//React Element
+// const heading = React.createElement("h1", { id: "heading" }, "Hello shital");
+
+//React element Using Jsx
+const heading = (
+  <h1 className="head" tabIndex="1">
+    Namaste React Foundation
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
-
-
+root.render(heading);
