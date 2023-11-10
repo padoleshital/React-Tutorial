6ef1c52b330b7685063ab2b1493075d7ffefa0d7
padoleshital <shitalpadole7@gmail.com>
2023-11-10 14:13:57 +0530

    episode-3 functional  component
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-// const heading = React.createElement("h1", { id: "heading" }, "Hello shital");
+const number = 1000;
+//React element
 
-//React element Using Jsx
 const heading = (
   <h1 className="head" tabIndex="1">
-    Namaste React Foundation
+    Namaste React Using JSX
   </h1>
 );
 
+// React Element
+const ele = <span>React element</span>
+const title = (
+  <h1 className="head" tabIndex="5">
+    {ele}                               
+  </h1>       
+)
+
+// React Functional element
+const ImageCompoent =() =>(
+    // <h4 id="heading">This is image tag</h4>
+    <img src="pexels-pixabay-268533.jpg" alt="nature" width="500" height="600"/>
+)
+
+const InputComponent = () => (
+   <input type="text"/>
+)
+
+const LinkComponent = () => {
+ return <a href="https://www.w3schools.com"> Visit School </a>
+}
+
+//component composition
+const HeadingCompoenet = () => (
+    <div id="container">     
+      {title}                                                               
+      <h2>{console.log("iowiuiwdhuhdsjdbshsb")}</h2>
+       <h1>{ 1000 }</h1>
+        <ImageCompoent/>
+        <InputComponent/> 
+        <LinkComponent/>
+        <h1 className="heading">Namaste React Using Functional component</h1>
+    </div>
+)
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingCompoenet/>);
