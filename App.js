52fd650d64cc12277b604278a050b5b5c8fedd20
padoleshital <shitalpadole7@gmail.com>
2023-11-13 22:08:28 +0530

    calling React component in different way
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- App.js
+++ App.js
@@ -4,43 +4,19 @@ import ReactDOM from "react-dom/client";
 const number = 1000;
 //React element
 
-const heading = (
+const Title =() => (
   <h1 className="head" tabIndex="1">
     Namaste React Using JSX
   </h1>
-);
+); 
 
-// React Element
-const ele = <span>React element</span>
-const title = (
-  <h1 className="head" tabIndex="5">
-    {ele}                               
-  </h1>       
-)
-
-// React Functional element
-const ImageCompoent =() =>(
-    // <h4 id="heading">This is image tag</h4>
-    <img src="pexels-pixabay-268533.jpg" alt="nature" width="500" height="600"/>
-)
-
-const InputComponent = () => (
-   <input type="text"/>
-)
-
-const LinkComponent = () => {
- return <a href="https://www.w3schools.com"> Visit School </a>
-}
-
+// React Functional element 
 //component composition
 const HeadingCompoenet = () => (
     <div id="container">     
-      {title}                                                               
-      <h2>{console.log("iowiuiwdhuhdsjdbshsb")}</h2>
-       <h1>{ 1000 }</h1>
-        <ImageCompoent/>
-        <InputComponent/> 
-        <LinkComponent/>
+      {Title()}                                                               
+        <Title></Title>
+        <Title/>
         <h1 className="heading">Namaste React Using Functional component</h1>
     </div>
 )
