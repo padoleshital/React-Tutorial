fb6eb2823b0a545019fb3f40a57a2443ef100cd4
padoleshital <shitalpadole7@gmail.com>
2023-11-14 17:56:38 +0530

    import image in react
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import pic from "./image/pexels-pixabay-268533.jpg";
 
 const number = 1000;
 //React element
@@ -12,14 +13,16 @@ const Title =() => (
 
 // React Functional element 
 //component composition
-const HeadingCompoenet = () => (
-    <div id="container">     
-      {Title()}                                                               
-        <Title></Title>
+const HeadingCompoenet = () => {
+  return (  
+  <div id="container">
+      <img className="logo" src={pic}  alt="logo"/>
         <Title/>
         <h1 className="heading">Namaste React Using Functional component</h1>
     </div>
-)
+   
+    )
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
